c816109f3f914fb9b0f2f086831732547e846efd
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-11-30 14:28:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 80
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'c.html'));
+})
+
+app.get('/b.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'b.html'));
+  })
+
+
+
+app.listen(port, () => {
+  console.log('Up')
+})
